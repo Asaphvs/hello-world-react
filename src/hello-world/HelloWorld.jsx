4b22355078db7bea2ea1e10598776e4597981d29
Laboratório 05 <lab05@esucri.edu>
2025-04-07 18:00:09 -0500

    tudo feito

---
+++ src/hello-world/HelloWorld.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function HelloWorld(){
+  
+    let [name, setName] = useState('');
+    let [message, setMessage] = useState('');
+
+    const handleHelloWorld =() => {
+        setMessage(`Hello World, ${name}!`);
+    };
+    const handleClear =() => {
+        setMessage('');
+        setName('');
+    };
+    const handleChangeInput =(evento) => {
+       setName(evento.target.value);
+    };
+
+    // Crtl-K + Ctrl-C - comenta / Ctrl-K Ctrl-U - descomenta
+    const handleKeyInput = (evento) => {
+            console.log(evento)
+            if(evento.key==='Escape'){
+                handleClear();
+            }else if(evento.key==='Enter'){
+                handleHelloWorld();
+            }
+    }
+
+    return (
+    <div>
+    <h1>Hello World!!</h1>
+    <input type="text"  onChange={handleChangeInput} onKeyUp={handleKeyInput} value={name}/>
+    <button onClick={handleHelloWorld}>HelloWorld</button>
+    <button onClick={handleClear}>Limpar</button>
+    {message && <p> {message}</p>}
+    </div>
+)   
+}
